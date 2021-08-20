e7de116e8855412426743321a762168a09cb250c
SRC0747 <RjSrc@0747>
2021-08-20 13:27:26 +0530

    added: Creating the Linked List having keys 30,70 and 56.

--- src/main/java/MyNodeTest.java
+++ src/main/java/MyNodeTest.java
@@ -1,23 +1,11 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-/*public class MyNodeTest {
-    public void given3NumbersWhenLinkShouldPassedLinkedListTest(){
-        MyNode<Integer> myFirstNode = new MyNode(56);
-        MyNode<Integer> mySecondNode = new MyNode(30);
-        MyNode<Integer> myThirdNode = new MyNode(70);
-        myFirstNode.setNext(mySecondNode);
-        mySecondNode.setNext(myThirdNode);
-        boolean result = myFirstNode.getNext().equals(mySecondNode) && myFirstNode.getNext().getNext().equals(myThirdNode);
-        Assert.assertTrue(result);
-    }
-}*/
-
-/*Creating A linked list changing the position of the nodes and in sequence of 70,30,56*/
+/*Creating A linked list changing the position of the nodes and in sequence of 30,70,56*/
 public class MyLinkedListTest {
     public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop(){
-        MyNode<Integer> myFirstNode = new MyNode<>(70);
-        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myFirstNode = new MyNode<>(30);
+        MyNode<Integer> mySecondNode = new MyNode<>(70);
         MyNode<Integer> myThirdNode = new MyNode<>(56);
         MyLinkedList myLinkedList = new MyLinkedList();
         myLinkedList.add(myFirstNode);
