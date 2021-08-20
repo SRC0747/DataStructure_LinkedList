d451bf73752170012ad045f6c669afbf8de8c7ed
SRC0747 <87673329+SRC0747@users.noreply.github.com>
2021-08-20 20:46:43 +0530

    Update MyNodeTest.java

--- src/main/java/MyNodeTest.java
+++ src/main/java/MyNodeTest.java
@@ -46,6 +46,24 @@ public class MyLinkedListTest {
 }
 
 @Test
+/*Inserting 30 between 56 and 70.*/
+public class MyLinkedListTest {
+    public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.insert(myFirstNode,mySecondNode);
+        boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
+}
+@Test
 /*Delete the first element of the Linked List in 56,30,70 Sequence.*/
 public class MyLinkedListTest {
     public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop() {
@@ -59,6 +77,7 @@ public class MyLinkedListTest {
         myLinkedList.add(myFirstNode);
         myLinkedList.append(myThirdNode);
         myLinkedList.insert(myFirstNode,mySecondNode);
+        myLinkedList.pop();
         boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myThirdNode);
         Assert.assertTrue(result);
     }
